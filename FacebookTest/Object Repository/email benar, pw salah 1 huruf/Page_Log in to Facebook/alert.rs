32d740a771dbee6c716b7a91c860142e9ab47eb5
LutfiSetioningtias <70835011+lutfiset@users.noreply.github.com>
2022-08-17 15:23:09 +0700

    Add files via upload

---
+++ FacebookTest/Object Repository/email benar, pw salah 1 huruf/Page_Log in to Facebook/alert.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>alert</name>
+   <tag></tag>
+   <elementGuidId>223c94b3-19e9-4031-afd8-c90209e651e9</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//body/div[@id='u_0_0_MA']/div[@id='globalContainer']/div[@id='content']/div[1]/div[2]/div[2]/form[1]/div[1]/div[2]/div[2]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
